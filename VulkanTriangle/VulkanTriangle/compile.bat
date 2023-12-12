60cde86a4054235bb861f6a0a5349b0c94d36b04
donghao.zhao <tony.donghao@gmail.com>
2023-12-12 22:47:42 +0800

    Preparing shader

---
+++ VulkanTriangle/VulkanTriangle/compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.3.268.0\Bin\glslc.exe .\shader\shader.vert -o .\shader\vert.spv
+C:\VulkanSDK\1.3.268.0\Bin\glslc.exe .\shader\shader.frag -o .\shader\frag.spv
+pause
--- VulkanTriangle/VulkanTriangle/main.cpp
+++ VulkanTriangle/VulkanTriangle/main.cpp
@@ -105,6 +105,7 @@ private:
 		createLogicalDevice();
 		createSwapChain();
 		createImageViews();
+		createGraphicsPipeline();
 	}
 
 	void mainLoop() {
@@ -340,6 +341,10 @@ private:
 				throw std::runtime_error("failed to create image views!");
 			}
 		}
+	}
+
+	void createGraphicsPipeline() {
+
 	}
 
 	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
