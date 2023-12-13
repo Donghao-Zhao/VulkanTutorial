7ae6506341a70c570cafa52734780c077392822b
donghao.zhao <tony.donghao@gmail.com>
2023-12-13 19:34:12 +0800

    Programmable stages of the pipeline

--- VulkanTriangle/VulkanTriangle/compile.bat
+++ VulkanTriangle/VulkanTriangle/compile.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.3.268.0\Bin\glslc.exe .\shader\shader.vert -o .\shader\vert.spv
-C:\VulkanSDK\1.3.268.0\Bin\glslc.exe .\shader\shader.frag -o .\shader\frag.spv
+C:\VulkanSDK\1.3.268.0\Bin\glslc.exe .\shaders\shader.vert -o .\shaders\vert.spv
+C:\VulkanSDK\1.3.268.0\Bin\glslc.exe .\shaders\shader.frag -o .\shaders\frag.spv
 pause
