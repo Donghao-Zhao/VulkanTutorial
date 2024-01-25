2de508d0f4cab64bfcb3445f6c048640b7d0f49d
Donghao Zhao <tony.donghao@gmail.com>
2024-01-25 18:35:29 +0800

    Vertex buffer creation

--- VulkanTriangle/VulkanTriangle/compile.bat
+++ VulkanTriangle/VulkanTriangle/compile.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.3.268.0\Bin\glslc.exe .\shaders\shader.vert -o .\shaders\vert.spv
-C:\VulkanSDK\1.3.268.0\Bin\glslc.exe .\shaders\shader.frag -o .\shaders\frag.spv
+C:\SDK\VulkanSDK\1.3.275.0\Bin\glslc.exe .\shaders\shader.vert -o .\shaders\vert.spv
+C:\SDK\VulkanSDK\1.3.275.0\Bin\glslc.exe .\shaders\shader.frag -o .\shaders\frag.spv
 pause
--- VulkanTriangle/VulkanTriangle/main.cpp
+++ VulkanTriangle/VulkanTriangle/main.cpp
@@ -172,6 +172,10 @@ private:
 
 	//指令池，管理指令缓冲对象使用的内存并负责指令缓冲对象的分配
 	VkCommandPool commandPool;
+
+	VkBuffer vertexBuffer;
+	VkDeviceMemory vertexBufferMemory;
+
 	//存储指令缓冲对象
 	std::vector<VkCommandBuffer> commandBuffers;
 
@@ -224,6 +228,7 @@ private:
 		createGraphicsPipeline();
 		createFramebuffers();
 		createCommandPool();
+		createVertexBuffer();
 		createCommandBuffers();
 		createSyncObjects();
 	}
@@ -261,9 +266,12 @@ private:
 
 		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
-
 		vkDestroyRenderPass(device, renderPass, nullptr);
+
+		vkDestroyBuffer(device, vertexBuffer, nullptr);
+		vkFreeMemory(device, vertexBufferMemory, nullptr);
 		
+		vkDestroyBuffer(device, vertexBuffer, nullptr);
 		//消除信号量
 		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
 			vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
@@ -876,6 +884,51 @@ private:
 		}
 	}
 
+	void createVertexBuffer() {
+		VkBufferCreateInfo bufferInfo{};
+		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+		bufferInfo.size = sizeof(vertices[0]) * vertices.size();
+		bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+		if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create vertex buffer!");
+		}
+
+		VkMemoryRequirements memRequirements;
+		vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
+
+		VkMemoryAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = memRequirements.size;
+		allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+		if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate vertex buffer memory!");
+		}
+
+		vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+
+		void* data;
+		vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
+			memcpy(data, vertices.data(), (size_t)bufferInfo.size);
+		vkUnmapMemory(device, vertexBufferMemory);
+	}
+
+	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
+		VkPhysicalDeviceMemoryProperties memProperties;
+		vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+		for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+			if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+				return i;
+			}
+		}
+
+		throw std::runtime_error("failed to find suitable memory type!");
+
+	}
+
 	//分配指令缓冲
 	void createCommandBuffers() {
 		//绘制操作是在帧缓冲上进行的
@@ -941,8 +994,12 @@ private:
 			//TODO；？？
 			vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
 
+			VkBuffer vertexBuffers[] = { vertexBuffer };
+			VkDeviceSize offsets[] = { 0 };
+			vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
+
 			//绘制三角形，提交绘制操作到指令缓冲
-			vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+			vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
 		
 		//结束渲染流程
 		vkCmdEndRenderPass(commandBuffer);
